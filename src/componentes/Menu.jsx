74cc410bf96bb86504c30bbd16774c1f101859ca
Gustavo Moura <gustavomouradejesus@gmail.com>
2025-09-26 15:01:18 -0300

    Reiniciando repositório após mover pasta

---
+++ src/componentes/Menu.jsx
@@ -0,0 +1,17 @@
+
+import styles from './Menu.module.css'
+
+function Menu() {
+    return(
+
+        <nav>
+            <a href="https://www.oracle.com">ORACLE</a>
+            <a href="https://www.bloombrtg.com">BLOOMBERG</a>
+            <a href="https://www.ibm.com">IBM</a>
+            <a href="https://www.google.com">GOOGLE</a>
+        </nav>
+
+    )
+}
+
+export default Menu;
